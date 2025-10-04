c142e11ba3dc10b572913883567ee79a61a7abbb
Daria <dashaemelianenko235@gmail.com>
2025-10-04 15:20:34 +0200

    refactor: remove skills section from header

--- react-frontend/src/components/Header/Header.js
+++ react-frontend/src/components/Header/Header.js
@@ -9,7 +9,6 @@ const Header = () => {
           <li><a href="#header">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#projects">Project</a></li>
-          <li><a href="#skill">Skill</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
